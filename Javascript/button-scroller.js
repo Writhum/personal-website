faadad61e09c05871d2c535dd923ab2472a5ae05
Writhum <geoashwebdesign@gmail.com>
2024-05-12 12:24:20 -0700

    Add files via upload

---
+++ Javascript/button-scroller.js
@@ -0,0 +1,32 @@
+document.getElementById("contact-info-button").addEventListener("click", function() {
+  // Define the height you want to scroll to
+  var scrollHeight = 2060; // Adjust this value as needed
+
+  // Scroll smoothly to the specified height
+  window.scrollTo({
+    top: scrollHeight,
+    behavior: "smooth"
+  });
+});
+
+document.getElementById("projects-button").addEventListener("click", function() {
+  // Define the height you want to scroll to
+  var scrollHeight = 953; // Adjust this value as needed
+
+  // Scroll smoothly to the specified height
+  window.scrollTo({
+    top: scrollHeight,
+    behavior: "smooth"
+  });
+});
+
+document.getElementById("about-me-button").addEventListener("click", function() {
+  // Define the height you want to scroll to
+  var scrollHeight = 2060; // Adjust this value as needed
+
+  // Scroll smoothly to the specified height
+  window.scrollTo({
+    top: scrollHeight,
+    behavior: "smooth"
+  });
+});
---
+++ Javascript/title-scale-limiter.js
@@ -0,0 +1,27 @@
+// JavaScript to toggle a class when the media query condition is met
+window.addEventListener('resize', function() {
+  var slidingElement = document.getElementById('geoffrey-title');
+  if (window.innerWidth <= 1456) {
+    slidingElement.classList.add('stopped');
+  } else {
+    slidingElement.classList.remove('stopped');
+  }
+});
+
+window.addEventListener('resize', function() {
+  var slidingElement = document.getElementById('ashton-title');
+  if (window.innerWidth <= 1490) {
+    slidingElement.classList.add('stopped');
+  } else {
+    slidingElement.classList.remove('stopped');
+  }
+});
+
+window.addEventListener('resize', function() {
+  var slidingElement = document.getElementById('bio-container');
+  if (window.innerWidth <= 1490) {
+    slidingElement.classList.add('stopped');
+  } else {
+    slidingElement.classList.remove('stopped');
+  }
+});
